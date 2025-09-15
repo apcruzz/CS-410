76a4c9863c9383b1c51dc88ea8fd04c51dc99d03
Andre Paul Cruz <andrepaulcruz29@gmail.com>
2025-09-15 00:32:05 +0000

    Debugging php

---
+++ src/sol.php
@@ -0,0 +1,27 @@
+<?php include("header.php"); ?>    
+    <h1 id="name">Andre Paul <br>Cruz</h1>
+    <h2 id="major">Computer Science Major | Arts and <br>Organizational Leadership Minor</h2>
+    <img id="my-image" src="images/andreC.png" alt="images/andrebg">
+    <p id="overview">Determined Computer Science major, passionate about technology to solve real-world problems. 
+        I am dedicated to continuous learning and innovation. My goal is to contribute to impactful projects and advance in the tech industry.</p>
+
+    <!-- must be removed just for problem solving -->
+    <!-- <?php
+    echo "Hello World";
+    ?> -->
+    <!-- must be removed just for problem solving -->
+    <p class="hello-world">
+        <?php echo "Hello World"; ?>
+    </p>
+
+    <?php
+    $name = $_GET['visitor'];
+    if ($name == "Andre"){ ?>
+        <p id="php">Welcome back, Andre!</p>
+    <?php } else { ?>
+        <p id="php">Hello, visitor!</p>
+    <?php
+
+    } ?>
+    
+<?php include("footer.php"); ?>
